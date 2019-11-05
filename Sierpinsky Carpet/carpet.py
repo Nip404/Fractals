bc226d9a36c071c39c616e58f4fc475a2030dede
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 23:56:14 +0000

    Update and rename main.py to carpet.py

--- Sierpinsky Carpet/carpet.py
+++ Sierpinsky Carpet/carpet.py
@@ -13,20 +13,20 @@ animation_delay = 0
 
 pygame.init()
 s = [window_width]*2
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Sierpinsky Carpet by NIP")
 
-def draw(centre,length,i,recursive_lim):
-    pygame.draw.rect(screen,[0]*3,(centre[0]-(length/2),centre[1]-(length/2),length,length),0)
+def draw(centre, length, i, recursive_lim):
+    pygame.draw.rect(screen, [0]*3, (centre[0] - (length/2), centre[1] - (length/2), length, length), 0)
 
     if i >= recursive_lim:
         return
 
     events()
 
-    for n in [[centre[0]-length,centre[1]-length],[centre[0],centre[1]-length],[centre[0]+length,centre[1]-length],[centre[0]-length,centre[1]],[centre[0]+length,centre[1]],[centre[0]-length,centre[1]+length],[centre[0],centre[1]+length],[centre[0]+length,centre[1]+length]]:
-        draw(n,length/3,i+1,recursive_lim)
+    for n in [[centre[0]-length, centre[1]-length], [centre[0], centre[1]-length], [centre[0]+length, centre[1]-length], [centre[0]-length, centre[1]], [centre[0]+length, centre[1]], [centre[0]-length, centre[1]+length], [centre[0], centre[1]+length], [centre[0]+length, centre[1]+length]]:
+        draw(n, length/3, i+1, recursive_lim)
 
         if animate:
             pygame.display.flip()
@@ -41,8 +41,8 @@ def events():
 def main():
     layers = 4
 
-    screen.fill((255,255,255))
-    draw([i/2 for i in s],s[0]/3,0,layers)
+    screen.fill((255, 255, 255))
+    draw([i/2 for i in s], s[0]/3, 0, layers)
     pygame.display.flip()
     
     while True:
