fb2b371a2137b16322d46f5891fa8f0e2fce56cc
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 23:54:20 +0000

    Update Engine.py

--- Tree/Engine.py
+++ Tree/Engine.py
@@ -2,9 +2,9 @@ import pygame
 import math
 
 class Tree:
-    def __init__(self,surface):
+    def __init__(self, surface):
         self.surf = surface
-        self.start = [self.surf.get_width()/2,self.surf.get_height()*0.7]
+        self.start = [self.surf.get_width()/2, self.surf.get_height()*0.7]
         self.length = self.surf.get_size()[0]/10
         self.maxfactor = 10
 
@@ -14,24 +14,23 @@ class Tree:
         self.decay = 1.4
         self.binary = True
 
-    def build_tree(self,pos,deg,length,recur=0):
+    def build_tree(self, pos, deg, length, recur=0):
         if not recur:
             return
 
-        heading_vector = [math.cos(math.radians(deg)),math.sin(math.radians(deg))]
-        new = [(heading_vector[i]*length)+pos[i] for i in range(2)]
+        heading_vector = [math.cos(math.radians(deg)), math.sin(math.radians(deg))]
+        new = [(heading_vector[i]*length) + pos[i] for i in range(2)]
         
-        pygame.draw.line(self.surf,(0,0,0),pos,new,1)
+        pygame.draw.line(self.surf, (0, 0, 0), pos, new, 1)
 
-        self.build_tree(new,deg-self.delta,length/self.decay,recur-1)
-        self.build_tree(new,deg+self.delta,length/self.decay,recur-1)
+        self.build_tree(new, deg - self.delta, length/self.decay, recur-1)
+        self.build_tree(new, deg + self.delta, length/self.decay, recur-1)
 
         if not self.binary:
-            self.build_tree(new,deg,length/self.decay,recur-1)
+            self.build_tree(new, deg, length/self.decay, recur-1)
 
     def draw_tree(self):
-        self.surf.fill((255,255,255))
-        self.build_tree(self.start,-90,self.length,self.factor)
+        self.surf.fill((255, 255, 255))
+        self.build_tree(self.start, -90, self.length, self.factor)
         pygame.display.flip()
         
-
